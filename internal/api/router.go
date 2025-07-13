7555c222cae49ffcf1e6b8a6680f13fdec665120
Rayberg <neformat30@gmail.com>
2025-07-13 23:34:18 +0300

    added endpoints in router.go

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,14 +1,38 @@
 package api
 
 import (
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/Danyarbrg/flashCards/internal/models"
+	"github.com/gin-gonic/gin"
 )
+
+var flashcards []models.Flashcard
+var idCounter int = 1
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+	// Get all the cards.
+	r.GET("/cards", func(c *gin.Context){
+		c.JSON(http.StatusOK, flashcards)
 	})
+
+	// Input the new card.
+	r.POST("/cards", func(c *gin.Context){
+		var newCard models.Flashcard
+
+		if err := c.ShouldBindJSON(&newCard); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		newCard.ID = idCounter
+		idCounter++
+
+		flashcards = append(flashcards, newCard)
+		c.JSON(http.StatusCreated, newCard)
+	})
+
 	return r
 }
